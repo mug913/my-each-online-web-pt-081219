3afb1df0465829f1020973503ae8019a5aca963e
Brad Alexander <brad.alexander913@gmail.com>
2019-08-15 01:53:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,7 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  count = 0 
+  while count < array.length 
+  puts array[count]
+  count += 1 
+end 
 end
