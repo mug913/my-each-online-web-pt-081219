aa378b6df4c89793403aaa09bc19de3d4147afb0
Brad Alexander <brad.alexander913@gmail.com>
2019-08-15 01:57:07 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,10 @@
 def my_each(array) # put argument(s) here
   count = 0 
+  new_array = []
   while count < array.length 
-  array[count]
+  puts array[count]
+  new_array << array[count]
   count += 1 
 end 
+new_array
 end
